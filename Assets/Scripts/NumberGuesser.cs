d5a60308322dc1857327fa7a8782ca3783bffb13
lowieCOOL <lowie.vang@gmail.com>
2021-03-31 13:12:24 +0200

    Intial commit

---
+++ Assets/Scripts/NumberGuesser.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NumberGuesser : MonoBehaviour
+{
+    [SerializeField] int maximumNumber;
+    [SerializeField] int minimumNumber;
+    int guess;
+    [SerializeField] TextMeshProUGUI guessText;
+    string stringedGuess;
+    int[] allGuesses;
+    int guessCount = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartGame();
+    }
+
+    void StartGame()
+    {
+        NextGuess();
+    }
+
+    public void LowerGuess()
+    {
+        maximumNumber = guess - 1;
+        NextGuess();
+    }
+
+    public void HigherGuess()
+    {
+        minimumNumber = guess + 1;
+        NextGuess();
+    }
+
+    void NextGuess()
+    {
+        guess = Random.Range(minimumNumber, maximumNumber + 1);
+        stringedGuess = guess.ToString();
+        guessText.text = stringedGuess;
+    }
+}
---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadNextScene ()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex + 1;
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+
+    public void LoadStartMenu()
+    {
+        int startSceneIndex = 0;
+        SceneManager.LoadScene(startSceneIndex);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
